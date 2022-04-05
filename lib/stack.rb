92429cd7570002b09e68d5fc0a9f904a7bc3951c
Olivia Pomeroy <opomeroy26@gmail.com>
2022-04-05 14:31:49 -0700

    commit tree 3cc1a9d0362968c80af80e264caf49f397a7e835

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,19 @@
 # your code here
+class Stack
+    def initialize 
+        @stack = []
+        @length = 0
+    end
+
+    def push(v) 
+        @stack.push(v)
+    end
+
+    def pop 
+        @stack.pop
+    end 
+
+    def peek 
+        @stack[-1]
+    end
+end
